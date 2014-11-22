e00704c3036191ce1d04f9faca16cf54ddfc18f5
ivan.butygin@gmail.com <ivan.butygin@gmail.com@7500a3ab-7df7-2f12-f995-7f2cbb4fa675>
2014-11-22 22:30:03 +0000

    intrusive ptr stub

---
+++ gamelib/memory/intrusiveptr.d
@@ -0,0 +1,84 @@
+﻿module gamelib.memory.intrusiveptr;
+
+import gamelib.memory.memory;
+
+struct IntrusivePtr(T) if (!is(T == class))
+{
+private:
+pure nothrow @safe:
+    T* payload = null;
+
+    void addRef()
+    {
+        if(isInitialized)
+        {
+            payload.addRef();
+        }
+    }
+    void release()
+    {
+        if(isInitialized)
+        {
+            payload.release();
+            payload = null;
+        }
+    }
+public:
+    @property
+    bool isInitialized() const
+    {
+        return payload !is null;
+    }
+
+    this(T* ptr)
+    {
+        payload  = ptr;
+    }
+
+    this(this)
+    {
+        addRef();
+    }
+
+    ~this()
+    {
+        release();
+    }
+
+    void opAssign(typeof(this) rhs)
+    {
+        if(rhs.payload is payload) return;
+        release();
+        payload = rhs.payload;
+        addRef();
+    }
+
+    void opAssign(T* rhs)
+    {
+        if(rhs is payload) return;
+        release();
+        payload = rhs;
+        addRef();
+    }
+
+    @property nothrow @safe
+    auto ptrPayload() inout
+    {
+        return payload;
+    }
+
+    @property nothrow @safe
+    auto ref refPayload() inout
+    {
+        assert(isInitialized, "Attempted to access an uninitialized payload.");
+        return *payload;
+    }
+
+    auto ref opUnary(string op : "*")()
+    {
+        return refPayload();
+    }
+
+    alias ptrPayload this;
+}
+
---
+++ gamelib/memory/memory.d
@@ -0,0 +1,34 @@
+﻿module gamelib.memory.memory;
+
+import std.traits;
+
+auto allocate(T,A...)(auto ref A args) if (!is(T == class))
+{
+    import core.memory : GC;
+    import core.stdc.stdlib : malloc;
+    import std.conv : emplace;
+    //import std.exception : enforce;
+    
+    auto ret = cast(T*) malloc(T.sizeof);
+    if(ret is null)
+    {
+        assert(false, "Allocation failed");
+    }
+    static if (hasIndirections!T)
+        GC.addRange(ret, T.sizeof);
+    emplace(ret, args);
+    return ret;
+}
+
+void deallocate(T)(T* ptr) if (!is(T == class))
+{
+    if(ptr is null) return;
+    .destroy(*ptr);
+    static if (hasIndirections!T)
+    {
+        import core.memory : GC;
+        GC.removeRange(&_refCounted._store._payload);
+    }
+    import core.stdc.stdlib : free;
+    free(ptr);
+}
