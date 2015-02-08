ab321a401e95723142be82b375b636a1a25e5643
ivan.butygin@gmail.com <ivan.butygin@gmail.com@7500a3ab-7df7-2f12-f995-7f2cbb4fa675>
2015-02-08 16:06:47 +0000

    range: adjacent

---
+++ gamelib/range.d
@@ -0,0 +1,18 @@
+﻿module gamelib.range;
+
+import std.range;
+
+auto adjacent(R)(R range)
+{
+    return zip(range,range.drop(1));
+}
+
+unittest
+{
+    import std.typecons;
+    assert(adjacent([1,2][0..0]).empty);
+    assert(adjacent([1]).empty);
+    assert(adjacent([1,2]).equal([tuple(1,2)]));
+    assert(adjacent([1,2,3]).equal([tuple(1,2),tuple(2,3)]));
+    assert(adjacent([1,2,3,4]).equal([tuple(1,2),tuple(2,3),tuple(3,4)]));
+}
