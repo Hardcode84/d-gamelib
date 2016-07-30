f87f3567faede72d9b806176cbf7aa0d4ea3845c
Ivan <ivan.butygin@gmail.com>
2016-07-30 17:28:32 +0300

    array bounds and switch error handlers

--- gamelib/asserthandler.d
+++ gamelib/asserthandler.d
@@ -6,44 +6,89 @@ import core.stdc.stdlib;
 
 version(GamelibHandleAssert)
 {
+pragma(inline, false):
 extern(C):
     void _d_assertm(immutable(ModuleInfo)* m, uint line)
     {
-        myAssertHandler(m.name, line);
+        myAssertHandler(m.name, line, 1);
     }
 
     void _d_assert_msg(string msg, string file, uint line)
     {
-        myAssertHandler(file, line, msg);
+        myAssertHandler(file, line, msg, 1);
     }
 
     void _d_assert(string file, uint line)
     {
-        myAssertHandler(file, line);
+        myAssertHandler(file, line, 1);
+    }
+
+    void _d_array_bounds(immutable(ModuleInfo)* m, uint line)
+    {
+        myRangeErrorHandler(m.name, line, 1);
+    }
+
+    void _d_arraybounds(string file, uint line)
+    {
+        myRangeErrorHandler(file, line, 1);
+    }
+
+    void _d_switch_error(immutable(ModuleInfo)* m, uint line)
+    {
+        mySwitchErrorHandler(m.name, line, 1);
     }
 }
 
-void printStacktrace()
+pragma(inline, false):
+void printStacktrace(size_t skipFrames) nothrow
 {
     try
     {
-        defaultTraceHandler.writeln;
+        foreach(i,const f; defaultTraceHandler)
+        {
+            if(i >= skipFrames)
+            {
+                writeln(f);
+            }
+        }
     }
     catch(Throwable t)
     {
-        writeln("Unable to print stacktrace: ", t);
+        try
+        {
+            writeln("Unable to print stacktrace: ", t);
+        }
+        catch(Throwable)
+        {
+        }
     }
 }
 
 private:
-void myAssertHandler(string file, size_t line)
+void mySwitchErrorHandler(string file, size_t line, size_t skipFrames) nothrow
 {
-    myAssertHandler(file, line, "Assertion failure");
+    myAssertHandler(file, line, "No appropriate switch clause found", skipFrames + 1);
 }
 
-void myAssertHandler(string file, size_t line, string msg)
+void myRangeErrorHandler(string file, size_t line, size_t skipFrames) nothrow
 {
-    writefln("%s(%s): %s", file, line, msg);
-    printStacktrace();
+    myAssertHandler(file, line, "Range violation", skipFrames + 1);
+}
+
+void myAssertHandler(string file, size_t line, size_t skipFrames) nothrow
+{
+    myAssertHandler(file, line, "Assertion failure", skipFrames + 1);
+}
+
+void myAssertHandler(string file, size_t line, string msg, size_t skipFrames) nothrow
+{
+    try
+    {
+        writefln("%s(%s): %s", file, line, msg);
+        printStacktrace(skipFrames + 1);
+    }
+    catch(Throwable)
+    {
+    }
     abort();
 }
